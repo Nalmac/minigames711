91e1df1938203234e99aa76fbbee369f549e2b46
Nalmac <simon.maciag@gmail.com>
2021-05-24 13:12:29 +0200

    Développement des modèles pour les échecs

--- Games/Chess/Models/Board.py
+++ Games/Chess/Models/Board.py
@@ -1,6 +1,14 @@
-from Games.Chess.Models import Square
+from Games.Chess.Models.Square import Square
 
 class Board():
     def __init__(self) -> None:
         self.files = ["a", "b", "c", "d", "e", "f", "g", "h"]
         self.rows = [i for i in range(1, 9)]
+        self.squares = {}
+
+        for i in self.files:
+            self.squares[i] = {}
+            for j in self.rows:
+                self.squares[i][j] = Square(i + str(j))
+
+        self.trait = 0
